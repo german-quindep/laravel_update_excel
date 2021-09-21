1b107989c84c0f5ac1725d246de3c46f95861c37
german-quindep <german.quindep@ug.edu.ec>
2021-09-21 17:24:44 -0500

    Se agrega rutas

--- routes/UploadExcel/excel.route.php
+++ routes/UploadExcel/excel.route.php
@@ -2,4 +2,4 @@
 //RUTAS PARA SUBIR EXCEL
 Route::get('/', 'UploadExcel@index')->name('index');
 Route::post('upload', 'UploadExcel@uploadFile')->name('uploadFile');
-Route::get('sendEmail', 'SendEmailController@sendEmailUser')->name('sendEmail');
+
